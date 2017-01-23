a3b086b21c10426f067819cc0b12e3e52ad07951
romankushalov <kushalov@ya.ru>
2017-01-24 01:08:01 +0300

    Update lesson4.js

--- lesson4.js
+++ lesson4.js
@@ -1,1 +1,9 @@
+// Задание 1. Функция минимального числа
+var a = +prompt("Введите число а:");
+var b = +prompt("Введите число b:");
+function min(a, b) {
+return (a < b) ? a : b;
+}
+console.log (min (a, b));
 
+// Задание 2. 
