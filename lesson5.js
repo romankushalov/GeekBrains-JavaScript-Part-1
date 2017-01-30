f790917c297e5be67b447f4666c0259dfc08e47f
romankushalov <kushalov@ya.ru>
2017-01-30 19:40:01 +0300

    Update lesson5.js

--- lesson5.js
+++ lesson5.js
@@ -1,1 +1,5 @@
-function rgb2hex(red, green, blue) {var rgb = blue | (green << 8) | (red << 16);return '#' + (0x1000000 + rgb).toString(16).slice(1)}
+prompt = 'Введите значение канала R:'
+function decimal2hex(r, g, b) {
+  var rgb = blue | (green << 8) | (red << 16);
+  return '#' + (0x1000000 + rgb).toString(16).slice(1)
+}
