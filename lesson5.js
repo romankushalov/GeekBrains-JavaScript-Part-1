567afe01e23f1b06f09b5345725bc0e1176dd6e0
romankushalov <kushalov@ya.ru>
2017-01-30 19:51:36 +0300

    Update lesson5.js

--- lesson5.js
+++ lesson5.js
@@ -1,5 +1,15 @@
-prompt = 'Введите значение канала R:'
+// Задание 1
+var r = +prompt('Введите значение канала R:');
+var g = +prompt('Введите значение канала G:');
+var b = +prompt('Введите значение канала B:');
+if ((r||g||b) > 255) {
+  alert ('Ошибка! Число должно быть в диапазоне 0 - 255!');
+} else {
 function decimal2hex(r, g, b) {
-  var rgb = blue | (green << 8) | (red << 16);
+  var rgb = r | (g << 8) | (b << 16);
   return '#' + (0x1000000 + rgb).toString(16).slice(1)
 }
+}
+decimal2hex(r, g, b);
+
+// Задание 2
