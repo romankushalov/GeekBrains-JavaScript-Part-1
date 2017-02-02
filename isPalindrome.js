407abc1950db5f79234e279af74e4842b43a1e86
romankushalov <kushalov@ya.ru>
2017-02-02 10:51:40 +0300

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -2,4 +2,6 @@ function isPalindrome (str) {
 var str=str.replace(/[^a-zA-Z0-9]+/gi,'').toLowerCase();
 return str==str.split('').reverse().join('');
 }
-isPalindrome("A car, a man, a maraca");
+console.log(isPalindrome("level")); // logs 'true'
+console.log(isPalindrome("levels")); // logs 'false'
+console.log(isPalindrome("A car, a man, a maraca")); // logs 'true'
